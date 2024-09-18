326c1eae07d20c1342174d4802daf5447e2d5d19
Jocelyn Nemba Pungwe <jocelynpungwe1997@gmail.com>
2024-09-18 18:49:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import BookingPage from './pages/BookingPage'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+import About from '../componants/about/About'
+import Contact from '../componants/contact/Contact'
+import Footer from '../componants/footer/Footer'
+import Hero from '../componants/hero/Hero'
+import Service from '../componants/service/Service'
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Service />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
